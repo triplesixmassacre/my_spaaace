88540d9dd81a49950254565ed82452fd98e2ed53
triplesixmassacre <o4kolom228@gmail.com>
2024-10-29 22:54:41 +0300

    Update rename.go

--- rename.go
+++ rename.go
@@ -3,15 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var n int
-	fmt.Scan(&n)
-	for n >= 10 {
-		sum := 0
-		for n > 0 {
-			sum += n % 10
-			n /= 10
-		}
-		n = sum
-	}
-	fmt.Println(n)
+	var a int
+	fmt.Scan(&a)
+	fmt.Println(a%10 + a/10%10 + a/100)
 }
